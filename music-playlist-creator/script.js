84957571ebd1d4d234a07f62b45bd1e0ecc8a26f
Eric Lin <ericlin0@meta.com>
2024-06-04 19:41:52 -0700

    Started website

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,17 @@
 
+// JavaScript for Opening and Closing the Modal
+var modal = document.getElementById("playlist-modal");
+var span = document.getElementsByClassName("close")[0];
+
+function openModal(festival) {
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
