e907435ecdcac08acdaa28deb6649af9c717a4f7
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2024-06-04 22:43:51 -0700

    More design update

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -3,13 +3,14 @@
 var modal = document.getElementById("playlist-modal");
 var span = document.getElementsByClassName("close")[0];
 
-function openModal(festival) {
+function openModal() {
    modal.style.display = "block";
 }
 
 span.onclick = function() {
    modal.style.display = "none";
 }
+
 window.onclick = function(event) {
    if (event.target == modal) {
       modal.style.display = "none";
